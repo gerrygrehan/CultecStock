d1c9bf7c438a937f63e18d7aaf3dcc7b668dd446
gerrygrehan <grehangerry@gmail.com>
2016-08-18 12:39:29 +0100

    Add files via upload

---
+++ Account/Login.aspx.vb
@@ -0,0 +1,29 @@
+﻿Imports Microsoft.AspNet.Identity
+Imports Microsoft.AspNet.Identity.EntityFramework
+Imports Microsoft.AspNet.Identity.Owin
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports Microsoft.Owin.Security
+
+Public Partial Class Account_Login
+    Inherits Page
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        
+    End Sub
+
+    Protected Sub LogIn(sender As Object, e As EventArgs)
+        If IsValid Then
+            ' Validate the user password
+            Dim manager = New UserManager()
+            Dim user As ApplicationUser = manager.Find(UserName.Text, Password.Text)
+            If user IsNot Nothing Then
+                IdentityHelper.SignIn(manager, user, RememberMe.Checked)
+                IdentityHelper.RedirectToReturnUrl(Request.QueryString("ReturnUrl"), Response)
+            Else
+                FailureText.Text = "Invalid username or password."
+                ErrorMessage.Visible = True
+            End If
+        End If
+    End Sub
+End Class
---
+++ Account/Register.aspx.vb
@@ -0,0 +1,28 @@
+﻿Imports Microsoft.AspNet.Identity
+Imports Microsoft.AspNet.Identity.EntityFramework
+Imports Microsoft.AspNet.Identity.Owin
+Imports System
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+
+Public Partial Class Account_Register
+    Inherits Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Not Me.Page.User.Identity.IsAuthenticated Then
+            FormsAuthentication.RedirectToLoginPage()
+        End If
+    End Sub
+    Protected Sub CreateUser_Click(sender As Object, e As EventArgs)
+        Dim manager = New UserManager()
+        Dim user = New ApplicationUser() With {.UserName = userName.Text}
+        Dim result = manager.Create(user, Password.Text)
+        If result.Succeeded Then
+            IdentityHelper.SignIn(manager, user, isPersistent:=False)
+            IdentityHelper.RedirectToReturnUrl(Request.QueryString("ReturnUrl"), Response)
+        Else
+            ErrorMessage.Text = result.Errors.FirstOrDefault()
+        End If
+    End Sub
+End Class
